a39e962307cee326cbefb7941b9857f29cc4ca8e
Pankaj Gurbani <pankajgurbani.connect@gmail.com>
2024-03-20 10:35:51 +0530

    Minor change

--- src/validation/notification.validation.js
+++ src/validation/notification.validation.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const getNotification = {
   query: Joi.object().keys({
     page: Joi.number().default(1),
-    limit: Joi.number().default(10),
+    limit: Joi.number().default(15),
   }),
 };
 
