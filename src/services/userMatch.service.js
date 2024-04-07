4700764f7dae9d259a8ef4e81b6e668971ac7a6f
Pankaj Gurbani <pankajgurbani.connect@gmail.com>
2024-04-07 13:33:00 +0530

    Code optimization

--- src/services/userMatch.service.js
+++ src/services/userMatch.service.js
@@ -2,7 +2,7 @@ import UserMatch from "../models/userMatch.model.js";
 
 /**
  * Create a usermatch document
- * @param {Object} userMatchBody
+ * @param {UserMatchSchema} userMatchBody
  */
 const createUserMatch = async (userMatchBody) => {
   return await UserMatch.create(userMatchBody);
@@ -11,10 +11,13 @@ const createUserMatch = async (userMatchBody) => {
 /**
  * Get user match by ID
  * @param {string} userMatchId
- * @return UserMatch document
  */
-const getUserMatchById = async (userMatchId) => {
-  return await UserMatch.findById(userMatchId);
+const getUserMatchById = (userMatchId) => {
+  return UserMatch.findById(userMatchId);
 }
 
-export { createUserMatch, getUserMatchById };
+export default { createUserMatch, getUserMatchById };
+  
+/**
+ * @typedef {import("../models/userMatch.model.js").UserMatchSchema} UserMatchSchema
+ */
